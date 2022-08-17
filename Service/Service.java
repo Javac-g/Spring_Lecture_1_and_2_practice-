e74430513581d69633aec5bb7e9bfa7493f972a1
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-08-17 20:49:17 +0300

    Method Print

---
+++ Service/Service.java
@@ -0,0 +1,14 @@
+package com.company.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Service {
+    private List<ResponseDTO> datalist = new ArrayList<>();
+
+
+    public String Print(String msg){
+        return msg;
+    }
+
+}
