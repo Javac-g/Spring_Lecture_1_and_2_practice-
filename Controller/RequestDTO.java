884f8e477a54ff13deccfe41e29a6ee4431e0232
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-08-17 20:36:53 +0300

    Car

---
+++ Controller/RequestDTO.java
@@ -0,0 +1,10 @@
+package com.company.Controller;
+
+import java.util.ArrayList;
+
+public class RequestDTO {
+    private String name;
+    private Integer id;
+    private Tool tool;
+    private List<Car> carlist = new ArrayList<>();
+}
